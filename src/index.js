762761034b25acea2c77e0388304f3447492d979
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2020-11-14 18:22:56 +0500

    Render Multiple Elements inside ReactDOM by <div/>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>We can wrapp various jsx element in div</p>
+  </div>,
+  document.getElementById("root")
+);
