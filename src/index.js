8c5f3782497ee07e9ee8f96ae95501c78784e1ed
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2020-11-14 18:32:33 +0500

    React.Fragment

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  [
-    <h1>Hello World</h1>,
+  <React.Fragment>
+    <h1>Hello World</h1>
     <p>
       Render Multiple Elements inside ReactDOM by using array in React above 16
-    </p>,
-  ],
+    </p>
+  </React.Fragment>,
   document.getElementById("root")
 );
