84085db236f0193f9491f1b8fc1e687173f829b3
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2020-11-15 10:57:11 +0500

    JSX Attributes  updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const imgLink = "/asset/imgs/sharjeel.jpg";
+const imgLink = [
+  {
+    id: 1,
+    url: "https://picsum.photos/200/300",
+  },
+  {
+    id: 2,
+    url: "https://picsum.photos/210/300",
+  },
+  {
+    id: 3,
+    url: "https://picsum.photos/220/300",
+  },
+];
 
 ReactDOM.render(
   <React.Fragment>
-    <img src={imgLink} alt="sharjeel" />
+    <h1>Image Gallery</h1>
+    <img src={imgLink[0].url} alt="random" />
+    <img src={imgLink[1].url} alt="random" />
+    <img src={imgLink[2].url} alt="random" />
   </React.Fragment>,
   document.getElementById("root")
 );
