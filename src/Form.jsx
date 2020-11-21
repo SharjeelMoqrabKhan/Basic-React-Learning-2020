949a3c93455df2f51ea92a4ad20572a1c908baef
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2020-11-21 12:49:29 +0500

    updated form

--- src/Form.jsx
+++ src/Form.jsx
@@ -8,15 +8,18 @@ const Form = () => {
   return (
     <div>
       <h1 style={{ color: "white" }}>Hi {fullName}</h1>
-      <input type="text" onChange={(e) => setname(e.target.value)} />
-      <button
-        style={{ margin: "20px" }}
-        onClick={() => {
-          setFullName(name);
-        }}
-      >
-        Say Hi
-      </button>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="text" onChange={(e) => setname(e.target.value)} />
+        <button
+          type="submit"
+          style={{ margin: "20px" }}
+          onClick={() => {
+            setFullName(name);
+          }}
+        >
+          Say Hi
+        </button>
+      </form>
     </div>
   );
 };
