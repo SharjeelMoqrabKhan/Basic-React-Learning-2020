0c9fcb820c5759bd6832a52271f243ea4d50522e
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2020-11-21 14:11:23 +0500

    Complex Multiple Input Form States

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,21 +2,50 @@ import React, { useState } from "react";
 import "../src/event.css";
 
 const Form = () => {
-  const [name, setname] = useState("");
-  const [fullName, setFullName] = useState();
+  const [fullName, setFullname] = useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  const inputHandler = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    setFullname((preValue) => {
+      if (name === "firstName") {
+        return {
+          firstName: value,
+          lastName: preValue.lastName,
+        };
+      } else if (name === "lastName") {
+        return {
+          firstName: preValue.firstName,
+          lastName: value,
+        };
+      }
+    });
+  };
 
   return (
     <div>
-      <h1 style={{ color: "white" }}>Hi {fullName}</h1>
+      <h1 style={{ color: "white" }}>
+        Hi {fullName.firstName} {fullName.lastName}
+      </h1>
       <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" onChange={(e) => setname(e.target.value)} />
-        <button
-          type="submit"
-          style={{ margin: "20px" }}
-          onClick={() => {
-            setFullName(name);
-          }}
-        >
+        <input
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+          value={fullName.firstName}
+          onChange={inputHandler}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+          value={fullName.lastName}
+          onChange={inputHandler}
+        />
+        <button type="submit" style={{ margin: "20px" }}>
           Say Hi
         </button>
       </form>
