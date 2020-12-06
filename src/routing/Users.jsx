18d377b54a1f78336ef251cbb952c047f54974d2
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2020-12-06 12:39:43 +0500

    uselocation hook

--- src/routing/Users.jsx
+++ src/routing/Users.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { Menu } from "./Menu";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 const Users = () => {
   const { firstName, lastName } = useParams();
+  const location = useLocation();
   return (
     <React.Fragment>
       <Menu />
       <h1>
         Greeting {firstName} {lastName}
       </h1>
+      <h2>URL {location.pathname}</h2>
+      {location.pathname === `/user/sharjeel/riaz` ? (
+        <button
+          onClick={() => {
+            alert("you are perimium user");
+          }}
+        >
+          Verify
+        </button>
+      ) : null}
     </React.Fragment>
   );
 };
